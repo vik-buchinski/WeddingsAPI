6a5c90534d809700cc363f15560f05cb81f55bcd
Viktor <vik-buchinski@ya.ru>
2015-05-06 20:42:43 +0300

    Started work at file upload

---
+++ WeddingAPI/Controllers/AboutController.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WeddingAPI.DAL;
+using WeddingAPI.Utils;
+
+namespace WeddingAPI.Controllers
+{
+    [RoutePrefix("api/admin/about")]
+    [EnableCors(origins: Constants.CLIENT_URL, headers: "*", methods: "*")]
+    public class AboutController : ApiController
+    {
+        private readonly Repositories _dataRepositories = new Repositories();
+
+        [Route("save")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> PostFile()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            if (!Directory.Exists(Constants.ABOUT_UPLOADS_PATH))
+            {
+                Directory.CreateDirectory(Constants.ABOUT_UPLOADS_PATH);
+            }
+
+            string root = HttpContext.Current.Server.MapPath(Constants.ABOUT_UPLOADS_PATH);
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                var sb = new StringBuilder(); // Holds the response body
+
+                // Read the form data and return an async task.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                //provider.FormData.Get("caption");
+
+                // This illustrates how to get the file names for uploaded files.
+                foreach (var file in provider.FileData)
+                {
+                    FileInfo fileInfo = new FileInfo(file.LocalFileName);
+                    sb.Append(string.Format("Uploaded file: {0} ({1} bytes)\n", fileInfo.Name, fileInfo.Length));
+                }
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(sb.ToString())
+                };
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _dataRepositories.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
