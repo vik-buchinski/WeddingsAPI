49eda9eebe3b6218b8dac4b701e4c98b42cf8c19
Buchinskij Viktor <vik-buchinski@ya.ru>
2015-07-07 09:52:32 +0300

    Added bouquets controller

---
+++ WeddingAPI/Controllers/UserPart/BouquetsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WeddingAPI.DAL;
+using WeddingAPI.Utils;
+
+namespace WeddingAPI.Controllers.UserPart
+{
+    [RoutePrefix("api")]
+    [EnableCors(origins: Constants.CLIENT_URL, headers: "*", methods: "*")]
+    public class BouquetsController : ApiController
+    {
+        private readonly Repositories _dataRepositories = new Repositories();
+
+        [Route("bouquets")]
+        [HttpGet]
+        public HttpResponseMessage GetBouquets()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+                Common.GetBouquetImages(_dataRepositories, Request.RequestUri.GetLeftPart(UriPartial.Authority)));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _dataRepositories.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- WeddingAPI/Utils/Common.cs
+++ WeddingAPI/Utils/Common.cs
@@ -1,14 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using WeddingAPI.DAL;
+using WeddingAPI.Models.Requests.Admin.Bouquets;
 
 namespace WeddingAPI.Utils
 {
     public class Common
     {
-
         public static String GenerateImageLink(int imageId, String leftUrlPart)
         {
             return leftUrlPart +
                    Constants.IMAGE_DOWNLOAD_URL + imageId;
+        }
+
+        public static List<BouquetImageModel> GetBouquetImages(Repositories dataRepositories, String leftUrlPart)
+        {
+            var bouquets = new List<BouquetImageModel>();
+            var images =
+                dataRepositories.ImagesModelRepository.Get(
+                    f => f.AlbumType.Equals(Constants.AlbumTypes.BOUQUETS.ToString()));
+            if (images == null)
+            {
+                return null;
+            }
+            foreach (var image in images)
+            {
+                bouquets.Add(new BouquetImageModel
+                             {
+                                 Id = image.Id,
+                                 Description = image.Description,
+                                 ImageUrl = GenerateImageLink(image.Id,
+                                     leftUrlPart)
+                             });
+            }
+            return bouquets;
         }
     }
 }
