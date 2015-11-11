d61bec36cc6cebd26ffbce80554bb0d26a211abb
Viktor <vik-buchinski@ya.ru>
2015-11-11 22:18:53 +0300

    Configurations for deploying

--- WeddingAPI/Migrations/Configuration.cs
+++ WeddingAPI/Migrations/Configuration.cs
@@ -1,3 +1,4 @@
+using WeddingAPI.Models.Database.Admin.About;
 using WeddingAPI.Models.Database.Auth;
 using WeddingAPI.Utils;
 
@@ -5,16 +6,16 @@ namespace WeddingAPI.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<WeddingAPI.DAL.WeddingContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.WeddingContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(WeddingAPI.DAL.WeddingContext context)
+        protected override void Seed(DAL.WeddingContext context)
         {
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -22,12 +23,16 @@ namespace WeddingAPI.Migrations
             //
             var password = "4205distum4205";
             var passwordHash = PasswordHash.CreateHash(password);
-            context.UserTable.AddOrUpdate(p => p.Id,
+            context.UserTable.AddOrUpdate(u => u.Email,
                 new UserModel { Email = "vik-buchinski@ya.ru", PasswordHash = passwordHash }
                );
-            context.UserTable.AddOrUpdate(p => p.Id,
-                new UserModel { Email = "marinagrishel@gmail.com", PasswordHash = "granada01" }
+            context.UserTable.AddOrUpdate(u => u.Email,
+                new UserModel { Email = "marinagrishel@gmail.com", PasswordHash = PasswordHash.CreateHash("granada01") }
                );
+            /*
+            context.AdminAboutTable.AddOrUpdate(a => a.Description,
+                new AdminAboutModel { Description = "this is test description"}
+               );*/
             context.SaveChanges();
         }
     }
